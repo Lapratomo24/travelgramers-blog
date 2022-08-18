2ef2d90b90a0dca0bdc0542dcf8c1ccd515bb605
Lapratomo24 <lapratomo24@yahoo.com>
2022-08-18 14:08:50 +0000

    create models and migrate to database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    """
+    Django model for creating posts
+    """
+    title = models.CharField(max_length=140, unique=True)
+    slug = models.SlugField(max_length=140, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+        )
+    updated_on = models.DateTimeField(auto_now=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    description = models.TextField()
+    attractions = models.TextField()
+    duration = models.CharField(max_length=10, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
+
+    class Meta:
+        """
+        Class to order posts in descending order
+        """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """
+        Method used to return the title
+        """
+        return self.title
+    
+    def number_of_likes(self):
+        """
+        Method used to count the number of likes on a post
+        """
+        return self.likes.count()
+
+
+class Comments(models.Model):
+    """
+    Django model for creating comments
+    """
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments'
+        )
+    name = models.CharField(max_length=140)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        """
+        Class to order posts in descending order
+        """
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
